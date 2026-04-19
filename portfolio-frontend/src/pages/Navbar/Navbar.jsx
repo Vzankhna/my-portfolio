c0eb00022afc2a1308a638570766184d3256aa17
Vzankhna <zankhnavaghela2@gmail.com>
2026-04-19 21:22:15 +0530

    Updated the Navbar page

--- portfolio-frontend/src/pages/Navbar/Navbar.jsx
+++ portfolio-frontend/src/pages/Navbar/Navbar.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-     <nav className="navbar">
-      <img src={logo} alt="Logo" />
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/skill">Skill</Link>
-        <Link to="/experience">Experience</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/contact">Contact</Link>
+    <nav className="navbar">
+      {/* Logo */}
+      <img src={logo} alt="Logo" className="logo" />
+
+      {/* Hamburger Icon */}
+      <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? "✖" : "☰"}
+      </div>
+
+      {/* Nav Links */}
+      <div className={`nav-links ${menuOpen ? "active" : ""}`}>
+        <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+        <Link to="/about" onClick={() => setMenuOpen(false)}>About</Link>
+        <Link to="/skill" onClick={() => setMenuOpen(false)}>Skill</Link>
+        <Link to="/experience" onClick={() => setMenuOpen(false)}>Experience</Link>
+        <Link to="/projects" onClick={() => setMenuOpen(false)}>Projects</Link>
+        <Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
