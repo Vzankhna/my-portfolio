db225fc41c29ebfc1b691c95cf71e06f47b4a37a
VZankhna <zankhnavaghela2@gmail.com>
2026-09-02 14:44:39 +0530

    Remove Unnecessary backend file

--- portfolio-backend/backend/portfolio/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Project, Contact, Experience
-
-admin.site.register(Project)
-admin.site.register(Contact)
-admin.site.register(Experience)
--- portfolio-backend/backend/portfolio/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('projects/', ProjectList.as_view()),
-    path('contact/', ContactCreate.as_view()),
-    path('experience/', ExperienceList.as_view()),
-]
